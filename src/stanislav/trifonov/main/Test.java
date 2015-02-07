f75df395eed75e362783893bf4588601ef3cee1f
Stanislav Trifonov <stan.trifonov@gmail.com>
2015-02-07 18:40:05 +0200

    A java library for the TextRank algorithm - initial commit

---
+++ src/stanislav/trifonov/main/Test.java
@@ -0,0 +1,10 @@
+package stanislav.trifonov.main;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/stanislav/trifonov/textrank/TextRank.java
@@ -0,0 +1,15 @@
+package stanislav.trifonov.textrank;
+
+import java.io.File;
+
+public class TextRank {
+
+	
+	public void feedText(String largeText) {
+		
+	}
+	
+	public void feedTextFromFile(File f) {
+		
+	}
+}
