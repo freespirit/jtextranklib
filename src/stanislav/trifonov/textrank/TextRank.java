5cfce8683acc3fb7ea44ae603e565959554c9c38
Stanislav Trifonov <stan.trifonov@gmail.com>
2015-02-08 19:53:26 +0200

    Working TextRank. Needs evaluation and adjustments(?)

--- src/stanislav/trifonov/textrank/TextRank.java
+++ src/stanislav/trifonov/textrank/TextRank.java
@@ -1,15 +1,145 @@
 package stanislav.trifonov.textrank;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class TextRank {
 
+	protected static final String WORD_REGEX = "\\w+";
+	protected static final String SENTENCE_SPLITTER_REGEX = "\\b+";
 	
-	public void feedText(String largeText) {
+	protected static final int MIN_SUMMARY_SENTENCES = 1;
+	protected static final int DEFAULT_SUMMARY_LENGTH_PERCENT = 10;
+	
+	protected static final float ERROR_RATE_THRESHOLD = 0.0001f;
+	protected static final float DAMPING_FACTOR = 0.85f;
+	
+	
+	private final List<String> _sentences;
+	/*
+	 * for each pair of edges place the weight/similarity on the crossing point
+	 * (i.e. sentence 9 and 4 have w=0.15, so [9][4] = [4][9] = 0.15)
+	 * 
+	 */
+	private final float _weight[][];
+
+	
+	public TextRank(List<String> sentences) {
+		_sentences = new ArrayList<String>(sentences);
+		_weight = new float[_sentences.size()][_sentences.size()];
+	}
+
+	public List<String> summarize(int summarySize) {
+		float similarity = 0.0f;
+		for(int i=0; i<_sentences.size()-1; ++i) {
+			for(int j=i+1; j<_sentences.size(); ++j) {
+				similarity = similarity( _sentences.get(i), _sentences.get(j) );
+				if(similarity > 0f)
+					_weight[i][j] = _weight[j][i] = similarity;
+			}
+		}
 		
+		for(int i=6; i</*_weight.length*/7; ++i) {
+			System.out.print((i+3) + ": ");
+			for(int j=0; j<_weight[i].length; ++j) {
+				System.out.print(String.format("%.2g\t", _weight[i][j]));
+			}
+			
+			System.out.println("");
+		}
+		
+		float scores[] = new float[_sentences.size()];
+		for (int i = 0; i < scores.length; i++)
+			scores[i] = 1f;
+		float score = 0f;
+		
+		float errorRate = Float.MAX_VALUE;
+		float vertexOutWeights = 0f;
+		
+		while(errorRate > ERROR_RATE_THRESHOLD || errorRate < (-ERROR_RATE_THRESHOLD)) {
+			for(int i=0; i<_sentences.size(); ++i) {
+				score = 0f;
+				for(int j=0; j<_sentences.size(); ++j) {
+					vertexOutWeights = 0f;
+					if(_weight[j][i] != 0f) {
+						for(int k=0; k<_sentences.size(); ++k)
+							vertexOutWeights += _weight[j][k];
+						
+						score += (_weight[j][i] / vertexOutWeights) * scores[j];
+					}
+				}
+				
+				score = score*DAMPING_FACTOR + (1-DAMPING_FACTOR);
+				errorRate = scores[i] - score;
+				scores[i] = score;
+			}
+		}
+		
+		
+		
+		Integer indices[] = new Integer[_sentences.size()];
+		for(int i=0; i<_sentences.size(); ++i)
+			indices[i] = i;
+		
+		Arrays.sort(indices, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return Float.compare(scores[o1], scores[o2]);
+			}
+		});
+		
+		
+		
+		int numberOfSentencesInSummary = Double.
+				valueOf(Math.ceil(indices.length*summarySize/100.0)).
+				intValue();
+		List<String> summary = new ArrayList<String>();
+		for (int i=indices.length-1; i>=/*indices.length-numberOfSentencesInSummary*/0; i--) {
+			System.out.println(scores[indices[i]] + " for " + _sentences.get(indices[i]));
+			summary.add( _sentences.get(indices[i]) );
+		}
+		
+		return summary;
+	}
+
+	//if similarity is 0 (the number of common words is 0), the 2 sentences are not connected in the graph
+	protected float similarity(String sentence1, String sentence2) {
+		if( sentence1 == null || sentence2 == null ||
+				sentence1.length() == 0 || sentence2.length() == 0 )
+			return 0.0f;
+		
+		String words1[] = wordsInSentence(sentence1);
+		String words2[] = wordsInSentence(sentence2);
+		
+		int commonWordsCount = 0;
+		for(int i=0; i<words1.length; ++i) {
+			for(int j=0; j<words2.length; ++j) {
+				if(words1[i].equals(words2[j]))
+					++commonWordsCount;
+			}
+		}
+		
+		return commonWordsCount /
+				(float)(Math.log(words1.length) + Math.log(words2.length));
 	}
 	
-	public void feedTextFromFile(File f) {
+	
+	protected String[] wordsInSentence(String sentence) {
+		Matcher wordMatcher = Pattern.compile(WORD_REGEX).matcher(sentence);
+		List<String> words = new ArrayList<String>();
+		String word;
 		
+		while(wordMatcher.find()) {
+			word = wordMatcher.group(0);
+			if(word != null)
+				words.add(word);
+		}
+		
+		return words.toArray( new String[words.size()] );
 	}
 }
